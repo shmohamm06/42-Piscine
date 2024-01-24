f589b4eda82f71089e423567ff01fb9fbce13355
Shahriar Shadman Mohammad <138768347+shmohamm06@users.noreply.github.com>
2024-01-24 10:24:20 +0400

    Update fizzbuzz.c

--- 42-Exams/ex01/fizzbuzz.c
+++ 42-Exams/ex01/fizzbuzz.c
@@ -26,21 +26,13 @@ int	main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			write(1, "fizzbuzz", 8);
-		}
 		else if (i % 3 == 0)
-		{
 			write(1, "fizz", 4);
-		}
 		else if (i % 5 == 0)
-		{
 			write(1, "buzz", 4);
-		}
 		else if (i < 10)
-		{
 			ft_putchar(i + '0');
-		}
 		else if (i >= 10)
 		{
 			ft_putchar('0' + i / 10);
